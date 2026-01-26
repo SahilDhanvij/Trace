93a04eb95df79448b1bc3c97afc80e1a222d7117
Sahil Dhanvij <sjdhanvij1998@gmail.com>
2026-01-26 22:42:06 +0530

    Google Authentication base code

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-
+import { UserModule } from 'src/user/user.module';
+import { googleService } from './google/googleService';
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, googleService],
+  imports : [UserModule]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { googleService } from './google/googleService';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly googleService : googleService,
+        private readonly userService : UserService
+    ){}
+
+    async loginWithGoogle(idToken : string){
+        const googleUser = await this.googleService.verify(idToken);
+        const user = await this.userService.findOrCreateByGoogleid(googleUser);
+        return {
+            user,
+        } 
+    }
+}
