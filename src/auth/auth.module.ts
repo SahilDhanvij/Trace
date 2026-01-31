7a9b8c35c59efb26a1fd3f1e760e76729bd28b3d
Sahil Dhanvij <sjdhanvij1998@gmail.com>
2026-01-31 15:27:00 +0530

    changes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
 import { googleService } from './google/googleService';
 import { JwtService } from './jwt/jwt.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategyService } from './jwt/jwt-strategy.service';
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, googleService, JwtService],
-  imports : [UserModule]
+  providers: [AuthService, googleService, JwtService, JwtStrategyService],
+  imports : [UserModule, JwtModule.register({})]
 })
 export class AuthModule {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { UserService } from './user.service';
 import { OAuthDTO } from 'src/DTO/oAuthDTO';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
 
 @Controller('user')
 export class UserController {
     constructor(
         private readonly userService : UserService
     ){}
+
+    @UseGuards(JwtAuthGuard)
+    @Get('me')
+    getMe(@Req() req){
+        return req.user;
+    }
 
     @Post('googleOauth')
     async googleLogin(@Body() oAuthDTO : OAuthDTO){
